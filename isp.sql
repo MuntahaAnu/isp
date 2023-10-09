3542b385774f134104088cdfdcecb27fd9656aa8
MuntahaAnu <lamiamuntaha1550@gmail.com>
2023-10-09 21:05:26 +0600

    some ui upgrade

--- isp.sql
+++ isp.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2018 at 12:52 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+-- Generation Time: Oct 06, 2023 at 06:42 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,150 +28,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int(25) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `ID` int(10) NOT NULL,
+  `username` int(10) NOT NULL,
+  `password` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `complain`
---
-
-CREATE TABLE `complain` (
-  `ID` int(25) NOT NULL,
-  `Complaint` varchar(255) NOT NULL,
-  `Date` varchar(30) DEFAULT NULL,
-  `Action` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `complain`
---
-
-INSERT INTO `complain` (`ID`, `Complaint`, `Date`, `Action`) VALUES
-(1, 'not internet in vashi', '2018-10-21', 'Solved'),
-(2, 'not int in pune 404', '2018-10-21', 'Solved'),
-(3, '607', '2018-10-21', 'Solved');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customer`
---
-
-CREATE TABLE `customer` (
-  `ID` int(25) NOT NULL,
-  `Name` varchar(255) NOT NULL,
-  `Contact` varchar(255) NOT NULL,
-  `Address` varchar(255) NOT NULL,
-  `Plan` varchar(255) DEFAULT NULL,
-  `Sex` varchar(255) NOT NULL,
-  `Purpose` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `customer`
---
-
-INSERT INTO `customer` (`ID`, `Name`, `Contact`, `Address`, `Plan`, `Sex`, `Purpose`) VALUES
-(11, 'nitish', '775z5df', 'csd', '10net', 'MALE', 'BUSINESS'),
-(12, 'rajas', '465165', 'dsfds', '100net', 'MALE', 'INDIVIDUAL');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employee`
---
-
-CREATE TABLE `employee` (
-  `ID` int(25) NOT NULL,
-  `Name` varchar(255) NOT NULL,
-  `Contact` varchar(255) NOT NULL,
-  `JoinDate` varchar(255) NOT NULL,
-  `Address` varchar(255) NOT NULL,
-  `LeaveDate` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `plan`
---
-
-CREATE TABLE `plan` (
-  `ID` int(25) NOT NULL,
-  `PlanName` varchar(255) NOT NULL,
-  `Cost` int(25) NOT NULL,
-  `Speed` varchar(25) NOT NULL,
-  `Duration` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `plan`
---
-
-INSERT INTO `plan` (`ID`, `PlanName`, `Cost`, `Speed`, `Duration`) VALUES
-(10, '10net', 110, '10', '6'),
-(100, '100net', 10000, '100', '10');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `complain`
---
-ALTER TABLE `complain`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `plan`
---
-ALTER TABLE `plan`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `complain`
---
-ALTER TABLE `complain`
-  MODIFY `ID` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+INSERT INTO `admin` (`ID`, `username`, `password`) VALUES
+(1, 1, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
